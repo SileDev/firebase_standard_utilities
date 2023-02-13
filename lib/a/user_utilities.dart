d4caf0635dffe72cf6294a97e1534e3e71633d58
SileDev <sdlpcuecem@gmail.com>
2023-02-13 00:14:54 -0500

    cambios

--- lib/a/user_utilities.dart
+++ lib/a/user_utilities.dart
@@ -1,5 +1,5 @@
+import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/cupertino.dart';
 
 class UserUtilities with ChangeNotifier {
   //Nombre de la colección que usará la clase
